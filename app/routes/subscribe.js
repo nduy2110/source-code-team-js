ab7dd73b46671dad11323b3006097e458cec7553
nduy2110 <92567616+nduy2110@users.noreply.github.com>
2022-11-10 14:47:23 +0700

    Delete app directory

--- app/routes/subscribe.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const subscribeController = require("../controllers/subscribe");
-
-const router = express.Router();
-
-router.post("/register", subscribeController.register);
-
-
-module.exports = router;
--- app/routes/user.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const express = require("express");
-const router = express.Router();
-
-const userController = require("../controllers/user");
-
-router.get("/",userController.information);
-router.post("/update", userController.update);
-module.exports = router;
